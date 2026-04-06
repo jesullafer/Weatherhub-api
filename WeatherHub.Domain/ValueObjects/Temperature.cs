298d77ef8f04ea84571982817832834a024c2569
Jesus Llanos <jesullafer@hotmail.com>
2026-04-06 12:17:52 +0100

    add behavior to Temperature value object

--- WeatherHub.Domain/ValueObjects/Temperature.cs
+++ WeatherHub.Domain/ValueObjects/Temperature.cs
@@ -2,9 +2,7 @@
 
 public sealed record Temperature
 {
-    public decimal Celsius { get; }
-
-    public bool IsFreezing => Celsius <= 0;
+    public decimal Celsius { get; }    
 
     public Temperature(decimal celsius)
     {
@@ -14,5 +12,14 @@ public sealed record Temperature
         Celsius = celsius;
     }
 
+    public bool IsFreezing => Celsius <= 0;
+    public bool IsHot => Celsius >= 30;
+
+    public decimal ToFahrenheit()
+    {
+        return (Celsius * 9 / 5) + 32;
+    }
+
     public override string ToString() => $"{Celsius:0.#} ºC";
+    
 }
