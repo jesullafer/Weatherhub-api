e995211a7d3e78186e057f81c351fd8aed02fa0c
Jesus Llanos <jesullafer@hotmail.com>
2026-04-06 11:16:15 +0100

    add temperature value object

---
+++ WeatherHub.Domain/ValueObjects/Temperature.cs
@@ -0,0 +1,18 @@
+﻿namespace WeatherHub.Domain.ValueObjects;
+
+public sealed record Temperature
+{
+    public decimal Celsius { get; }
+
+    public bool IsFreezing => Celsius <= 0;
+
+    public Temperature(decimal celsius)
+    {
+        if (celsius < -100 || celsius > 100)
+            throw new ArgumentOutOfRangeException(nameof(celsius), "Temperature must be between -100 and 100 ºC.");
+
+        Celsius = celsius;
+    }
+
+    public override string ToString() => $"{Celsius:0.#} ºC";
+}
